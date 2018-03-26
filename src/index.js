b315325b3af07b47af11f35c908d2a2c87d4bb3a
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2018-03-26 13:02:03 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import registerServiceWorker from './registerServiceWorker';
+
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(logger)
+  )
+);
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
