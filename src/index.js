f0df6e66d5d1af8ff1200bdea95caa89cf1cebfd
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2018-03-27 19:22:09 +0800

    fix type

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import GamesPage from './components/GamesPage';
 import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -8,6 +9,7 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
 const store = createStore(
   rootReducer,
@@ -18,7 +20,17 @@ const store = createStore(
 
 ReactDOM.render(
   <Provider store={ store }>
-    <App />
+    <Router>
+      <div className="ui container">
+        <div className="ui three item menu">
+          <NavLink exact activeClassName="active" className="item" to="/">Home</NavLink>
+          <NavLink exact activeClassName="active" className="item" to="/games">Games</NavLink>
+          <NavLink activeClassName="active" className="item" to="/games/new">Add New Game</NavLink>
+        </div>
+        <Route exact path="/" component={ App } />
+        <Route exact path="/games" component={ GamesPage } />
+      </div>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
