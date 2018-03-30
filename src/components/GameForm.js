84774c74066e0cdb8f0257a397f0270e28a0d2f1
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2018-03-30 15:45:47 +0800

    client validation

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
 
 class GameForm extends Component {
   state = {
     title: '',
-    cover: ''
+    cover: '',
+    errors: {}
   }
 
   handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+    if (!!this.state.errors[e.target.name]) {
+      let errors = Object.assign({}, this.state.errors);
+      delete errors[e.target.name];
+      this.setState({
+        [e.target.name]: e.target.value,
+        errors
+      });
+    } else {
+      this.setState({
+        [e.target.name]: e.target.value,
+      });
+    }
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
+
+    let errors = {};
+    if (this.state.title === '') errors.title = "Can't be empty";
+    if (this.state.cover === '') errors.cover = "Can't be empty";
+    this.setState({ errors });
   }
 
   render() {
@@ -19,7 +37,7 @@ class GameForm extends Component {
       <form className="ui form" onSubmit={ this.handleSubmit }>
         <h1>Add new game</h1>
 
-        <div className="field">
+        <div className={ classnames('field', { error: !!this.state.errors.title }) }>
           <label htmlFor="title">Title</label>
           <input
             type="text"
@@ -27,9 +45,10 @@ class GameForm extends Component {
             value={ this.state.title }
             onChange={ this.handleChange }
           />
+          <span>{ this.state.errors.title }</span>
         </div>
 
-        <div className="field">
+        <div className={ classnames('field', { error: !!this.state.errors.cover }) }>
           <label htmlFor="title">Cover Url</label>
           <input
             type="text"
@@ -37,6 +56,7 @@ class GameForm extends Component {
             value={ this.state.cover }
             onChange={ this.handleChange }
           />
+          <span>{ this.state.errors.cover }</span>
         </div>
 
         <div className="field">
